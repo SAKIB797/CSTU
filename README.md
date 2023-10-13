e7a0f18d075e9f7ac92c319b3be0c090dbc4636a
Iftekhar Sakib <pro.iftekhar@gmail.com>
2023-10-13 15:38:24 +0600

    everything done

---
+++ README.md
@@ -0,0 +1,6 @@
+# CSTU
+Demo website for Chandpur Science & Technology University - CSTU
+## Author Information
+:pencil2: Iftekhar Sakib  
+:arrow_right: [Email]( pro.iftekhar@gmail.com)  
+:arrow_right: [Facebook](https://www.facebook.com/SAKIB797)
---
+++ js/header.js
@@ -0,0 +1,12 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+  function openNotice() {
+    document.getElementById("myNotice").style.width = "100%";
+  }
+  function closeNotice() {
+    document.getElementById("myNotice").style.width = "0%";
+  }
